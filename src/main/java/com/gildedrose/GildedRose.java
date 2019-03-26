fe1fc306bb3270caacc7031700a83f5103605ac1
Anthony Malcourant <anthony.malcourant@contractor.voo.be>
2019-03-26 16:29:46 +0100

    Wemanity :: technical test :: fast renaming + loop with stream + use enum to store special items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,9 @@
 package com.gildedrose;
 
+import com.gildedrose.awake.SpecialItem;
+
+import java.util.stream.Stream;
+
 class GildedRose {
     Item[] items;
 
@@ -8,55 +12,60 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+        Stream.of(items)
+                .forEach(item -> {
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                    if (!item.name.equals(SpecialItem.AGED_BRIE.getName())
+                            && !item.name.equals(SpecialItem.TAFKAL80ETC_BACKSTAGE.getName())) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SpecialItem.SULFURAS.getName())) {
+                                item.quality--;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+
+                            if (item.name.equals(SpecialItem.TAFKAL80ETC_BACKSTAGE.getName())) {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality++;
+                                    }
+                                }
+
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality++;
+                                    }
+                                }
+                            }
+                        }
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+
+                    if (!item.name.equals(SpecialItem.SULFURAS.getName())) {
+                        item.sellIn--;
                     }
-                }
-            }
-        }
+
+                    if (item.sellIn < 0) {
+                        if (!item.name.equals(SpecialItem.AGED_BRIE.getName())) {
+                            if (!item.name.equals(SpecialItem.TAFKAL80ETC_BACKSTAGE.getName())) {
+                                if (item.quality > 0) {
+                                    if (!item.name.equals(SpecialItem.SULFURAS.getName())) {
+                                        item.quality--;
+                                    }
+                                }
+                            } else {
+                                item.quality = 0;
+                            }
+                        } else {
+                            if (item.quality < 50) {
+                                item.quality++;
+                            }
+                        }
+                    }
+
+                });
+
     }
 }
---
+++ src/main/java/com/gildedrose/awake/SpecialItem.java
@@ -0,0 +1,31 @@
+package com.gildedrose.awake;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public enum SpecialItem {
+
+    SULFURAS ("Sulfuras, Hand of Ragnaros"),
+    AGED_BRIE ("Aged Brie"),
+    TAFKAL80ETC_BACKSTAGE ("Backstage passes to a TAFKAL80ETC concert")
+    ;
+
+    private String name;
+
+    SpecialItem(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Optional<SpecialItem> fromName(final String name) {
+        return Stream.of(SpecialItem.values()).filter(item -> name.equals(item.getName())).findAny();
+    }
+
+    public static boolean exist(final String name) {
+        return fromName(name).isPresent();
+    }
+
+}
